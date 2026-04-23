772f163e547fd7269cf8c323d76dc95a60d23a46
dkswlsdn <dkswlsdn12@gmail.com>
2026-04-23 09:53:24 +0900

    텍스트 추가

--- Assets/Prefabs/Node.cs
+++ Assets/Prefabs/Node.cs
@@ -1,7 +1,15 @@
+using TMPro;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    public TextMeshProUGUI text;
+
     public int Height;
     public int Key;
+
+    private void Update()
+    {
+        text.text = Key.ToString();
+    }
 }
